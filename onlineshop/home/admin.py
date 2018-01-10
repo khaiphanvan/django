1858779847e3857fe81ad55231bff3e4b8a2a11d
khaiphanvan <khaipv.11@gmail.com>
2018-01-10 17:18:43 +0700

    commit 3

--- onlineshop/home/admin.py
+++ onlineshop/home/admin.py
@@ -1,3 +1,10 @@
+from django.apps import apps
 from django.contrib import admin
+from .models import *
 
+class cateAdmin(admin.ModelAdmin):
+    fields = ['cate_name','cate_description','cate_status']
+
+admin.site.register(Product)
+admin.site.register(Category,cateAdmin)
 # Register your models here.
