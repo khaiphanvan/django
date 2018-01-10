6c12cbb312f99b38883d12df259d9f72aea74a4f
khaiphanvan <khaipv.11@gmail.com>
2018-01-10 14:53:18 +0700

    display product

--- onlineshop/home/views.py
+++ onlineshop/home/views.py
@@ -3,7 +3,10 @@ from home.models import *
 # Create your views here.
 def index(request):
     cateParent = Category.objects.filter(cate_parent_id=0)
+    fetureItems = Product.objects.filter(product_status=1)[:6]
+    #categoryItems = Product.
     context = {
-        'parents' : cateParent
+        'parents' : cateParent,
+        'fetureItems' : fetureItems
     }
     return render(request, "home/index.html",context)
